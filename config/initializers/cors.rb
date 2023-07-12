36bb951450343b7a37f0f419b4399f549b83514c
Young Koh <youngheon6@gmail.com>
2023-07-12 11:02:50 -0600

    feat: add cors gem and configure cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,13 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'localhost:3000', 'your-react-app-domain.com'
+    resource '*', 
+        headers: :any, 
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
+  end
+end
