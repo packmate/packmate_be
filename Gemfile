8b75e824e0cf32f7a0a145a5c3192dc2564f72ce
Young Koh <youngheon6@gmail.com>
2023-07-13 14:52:22 -0600

    feat: add simplecov to monitor coverage

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ group :development, :test do
   gem 'faker'
   gem 'factory_bot_rails'
   gem 'shoulda-matchers'
+  gem 'simplecov'
 end
 
 group :development do
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,26 +1,13 @@
 module Types
   class QueryType < Types::BaseObject
-    # /users
-    field :users, [Types::UserType], null: false
 
-    def users
-      User.all
-    end
-
-    field :user, Types::UserType, null: false do
-      argument :id, ID, required: true
-    end
-
-    def user(id:)
-      User.find(id)
-    end
 
     field :items, [Types::ItemType], null: false do
       argument :category, String, required: true
     end
 
     def items(category:)
-      Item.joins(:tags).where(tags: {category: category})
+      Item.joins(:tags).where(tags: {category: category}).distinct
     end
   end
 end
